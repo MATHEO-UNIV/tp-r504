2228696760c8750af9d2161c4a9a977f0fa1e877
MATHEO-UNIV <matheofourneaux@icloud.com>
2024-09-16 17:07:15 +0200

    update ** 2

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,11 +1,21 @@
 def puissance(a, b):
     if not (type(a) is int and type(b) is int):
-        raise TypeError("Only integers are allowed")
+        raise TypeError("Seuls les nombres entiers sont autorisés")
+    
+    if b == 0:
+        return 1  # a^0 = 1 for any a
+    
+    if a == 0 and b < 0:
+        raise ValueError("0 ne peut être élevé à une puissance négative")
     
     result = 1
+    positive_b = abs(b)
 
-    for _ in range(b):
+    for _ in range(positive_b):
         result *= a
     
-    return result
+    if b < 0:
+        return 1 / result
+    else:
+        return result
 
