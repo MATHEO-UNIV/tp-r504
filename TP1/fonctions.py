f3b6b893c12a65341bd4fa80e9773ac0458c1005
MATHEO-UNIV <matheofourneaux@icloud.com>
2024-09-16 17:03:44 +0200

    update **

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,5 +1,11 @@
 def puissance(a, b):
     if not (type(a) is int and type(b) is int):
         raise TypeError("Only integers are allowed")
+    
+    result = 1
 
-    return a ** b
+    for _ in range(b):
+        result *= a
+    
+    return result
+
